d28e1aa9f2f85d250aeb10a28466f325f89a49ca
Lucas de Mello Vieira <lucas.vieira@ntl.labchecap.com.br>
2022-06-02 16:26:51 -0300

    Inicializando projeto

---
+++ src/components/Formulario.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Formulario para criação de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa voce deseja iniciar?">
+            </div>
+                <div class="column">
+                    <div class="is-flex is-align-items-center is-justify-content-space-between">
+                        <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
+                        <!-- <section>
+                            <strong>{{tempoDecorrido}}</strong>
+                        </section> -->
+                    <button class="button" @click="iniciar">
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>play</span>
+                    </button>
+                    <button class="button" @click="finalizar">
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>stop</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+<script lang="ts">
+
+    import { defineComponent } from "vue";
+    import Cronometro from "./Cronometro.vue";
+    export default defineComponent({
+    name: "Formulario",
+    components:{
+        Cronometro
+    },
+    data() {
+        return {
+            tempoEmSegundos: 0,
+            cronometro: 0
+        };
+    },
+    computed: {
+        tempoDecorrido(): string {
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substring(11, 19);
+        }
+    },
+    methods: {
+        iniciar() {
+            this.cronometro = setInterval(() => {
+                this.tempoEmSegundos++;
+            }, 1000);
+            console.log(this.tempoDecorrido);
+        },
+        finalizar() {
+            clearInterval(this.cronometro);
+        }
+    },
+})
+
+</script>
+
+
+
+<style lang="">
+    
+</style>
