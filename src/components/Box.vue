1640bc903849712ed17ef03c3e5552836a766028
Lucas de Mello Vieira <lucas.vieira@ntl.labchecap.com.br>
2022-06-06 06:53:46 -0300

    Finalizando terceira aula

---
+++ src/components/Box.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <div class="box has-text-wieght-bold">
+        <slot></slot>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue"
+export default defineComponent({
+    name:"Box"
+})
+</script>
+<style lang="">
+    
+</style>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -2,28 +2,11 @@
     <div class="box">
         <div class="columns">
             <div class="column is-8" role="form" aria-label="Formulario para criação de uma nova tarefa">
-                <input type="text" class="input" placeholder="Qual tarefa voce deseja iniciar?">
+                <input type="text" class="input" placeholder="Qual tarefa voce deseja iniciar?" v-model="descricao">
             </div>
                 <div class="column">
-                    <div class="is-flex is-align-items-center is-justify-content-space-between">
-                        <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
-                        <!-- <section>
-                            <strong>{{tempoDecorrido}}</strong>
-                        </section> -->
-                    <button class="button" @click="iniciar">
-                        <span class="icon">
-                            <i class="fas fa-play"></i>
-                        </span>
-                        <span>play</span>
-                    </button>
-                    <button class="button" @click="finalizar">
-                        <span class="icon">
-                            <i class="fas fa-play"></i>
-                        </span>
-                        <span>stop</span>
-                    </button>
+                    <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
                 </div>
-            </div>
         </div>
     </div>
 </template>
@@ -33,35 +16,28 @@
 <script lang="ts">
 
     import { defineComponent } from "vue";
-    import Cronometro from "./Cronometro.vue";
+    import Temporizador from "./Temporizador.vue";
     export default defineComponent({
-    name: "Formulario",
-    components:{
-        Cronometro
-    },
-    data() {
-        return {
-            tempoEmSegundos: 0,
-            cronometro: 0
-        };
-    },
-    computed: {
-        tempoDecorrido(): string {
-            return new Date(this.tempoEmSegundos * 1000).toISOString().substring(11, 19);
-        }
-    },
-    methods: {
-        iniciar() {
-            this.cronometro = setInterval(() => {
-                this.tempoEmSegundos++;
-            }, 1000);
-            console.log(this.tempoDecorrido);
+        name: "Formulario",
+        emits: ['aoSalvarTarefa'],
+        components:{
+            Temporizador
         },
-        finalizar() {
-            clearInterval(this.cronometro);
+        data(){
+            return{
+                descricao:''
+            }
+        },
+        methods:{
+            finalizarTarefa(tempoDecorrido: number) : void{
+                this.$emit('aoSalvarTarefa',{
+                    duracaoEmSegundos: tempoDecorrido,
+                    descricao: this.descricao
+                })
+                this.descricao = ''
+            }
         }
-    },
-})
+    })
 
 </script>
 
